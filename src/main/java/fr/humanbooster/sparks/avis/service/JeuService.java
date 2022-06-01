c4168565adb1e498904408733cfc57bd66b1907d
newenpoi <lammenschristopher119@hotmail.com>
2022-06-01 20:34:29 +0200

    recuperer jeu(x)

--- src/main/java/fr/humanbooster/sparks/avis/service/JeuService.java
+++ src/main/java/fr/humanbooster/sparks/avis/service/JeuService.java
@@ -1,5 +1,12 @@
 package fr.humanbooster.sparks.avis.service;
 
+import java.util.List;
+
+import fr.humanbooster.sparks.avis.business.Jeu;
+
 public interface JeuService {
+
+	Jeu recupererJeu(Long idJeu);
+	List<Jeu> recupererJeux();
 
 }
--- src/main/java/fr/humanbooster/sparks/avis/service/impl/JeuServiceImpl.java
+++ src/main/java/fr/humanbooster/sparks/avis/service/impl/JeuServiceImpl.java
@@ -1,7 +1,28 @@
 package fr.humanbooster.sparks.avis.service.impl;
 
-import fr.humanbooster.sparks.avis.service.JeuService;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import fr.humanbooster.sparks.avis.business.Jeu;
+import fr.humanbooster.sparks.avis.dao.JeuDao;
+import fr.humanbooster.sparks.avis.service.JeuService;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class JeuServiceImpl implements JeuService {
+	
+	private JeuDao jeuDao;
+
+	@Override
+	public Jeu recupererJeu(Long idJeu) {
+		return jeuDao.findById(idJeu).orElse(null);
+	}
+
+	@Override
+	public List<Jeu> recupererJeux() {
+		return jeuDao.findAll();
+	}
 
 }
